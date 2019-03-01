84ce0e498a5a7505366b44d278286ef2f2152e72
maximedupre <maximedupre@me.com>
2019-02-28 20:11:55 -0500

    Actually parametrize the function

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const jpeg = require('jpeg-js');
 
-module.exports = () => {
-	const width = 100;
-	const height = 100;
+module.exports = (width, height) => {
 	const data = new Buffer(width * height * 4);
 
 	return jpeg.encode({ data, width, height }).data;
