5cec70e3145bff889c8cf2bf32dad28321f5309a
maximedupre <maximedupre@me.com>
2019-03-08 14:54:18 -0500

    Makes image red instead of black for a less confusing dummy

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const jpeg = require('jpeg-js');
 
 module.exports = (width, height) => {
 	const data = new Buffer(width * height * 4);
+	let i = 0;
+
+	while (i < data.length) {
+		data[i++] = 0xFF;
+		data[i++] = 0x00;
+		data[i++] = 0x00;
+		data[i++] = 0xFF;
+	}
 
 	return jpeg.encode({ data, width, height }).data;
 }
